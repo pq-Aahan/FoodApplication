1006bc45937ff4bf53f6bb10761ecaafc5b0e91c
pq-Aahan <pulast.aahan@gmail.com>
2024-10-13 00:58:26 +0530

    completing

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,36 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Shimmer from './Shimmer';
+import { useParams } from 'react-router-dom';
+import { MENU_API } from '../utils/constants';
 
 const RestaurantMenu=()=> {
     const[resInfo,setResinfo]=useState([]);
+    const{resId}=useParams();
     useEffect (()=>{fetchMenu();},[]);
 
     const fetchMenu= async()=>{
-        const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7333148&lng=76.7794179&restaurantId=${resid}&catalog_qa=undefined&submitAction=ENTER");     
+        const data=await fetch(MENU_API+resId);     
         
         const json=await data.json();
         console.log("hi",json);
         setResinfo(json.data);
     };
 
-//const{name,cuisines,costForTwoMessage}=resInfo?.cards[2]?.card?.card?.info;
+//const{name,cuisines,costForTwoMessage}=resInfo?.cards[2]?.card?.card?.info; data.cards[4]..groupedcards.cardsgroupmap.regular.czrds[1]..card[0].ard.item.card.name
+
+const { name, cuisines, costForTwoMessage } = resInfo?.cards?.[2]?.card?.card?.info || {};
+const {itemCards} = resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card || [];
+
+if (!itemCards) {
+  console.log("itemCards is undefined or null");
+} else {
+  console.log("test", itemCards);
+}
 
 
 return !resInfo ? (
     <Shimmer />
 ) : (
     <div className="menu">
-        {resInfo?.cards?.length > 2 && (
-            <h1>{resInfo.cards[2]?.card?.card?.info?.name}</h1>
-        )}
-        {resInfo?.cards?.length > 2 && (
-            <>
-             <h3>{resInfo.cards[2].card.card.info.name}</h3>
-                <p>{resInfo.cards[2].card.card.info.cuisines.join(",")}</p>
-                <p>{resInfo.cards[2].card.card.info.costForTwoMessage}</p>
-            </>
-        )}
-    </div>
+            {resInfo?.cards?.length > 2 && (
+                <>
+                    <h1>{name}</h1>
+                    <h3>{name}</h3>
+                    <p>{cuisines?.join(", ")}-{costForTwoMessage}</p>
+                    <h2>MENU</h2>
+                    <ul>
+                        {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name}-{"Rs."}{item.card.info.price/100|| item.card.info.defaultPrice/100}</li>)}
+                        
+
+                    </ul>
+                </>
+                    )}
+        </div> 
 );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
+
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId="
